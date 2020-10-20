804a41bacdd6f8efbe552bc9dc3d2d0cbafecfa8
Mufeed Adam <gangolimufeed@gmail.com>
2020-10-20 15:51:37 +0530

    UI Changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { useState } from 'react'
 
 import { Jutsu } from 'react-jutsu'
+import './App.css'
 
 const App = () => {
   const [room, setRoom] = useState('')
   const [name, setName] = useState('')
   const [call, setCall] = useState(false)
   const [password, setPassword] = useState('')
-
+  const [message,setMessage] = useState('')
   const handleClick = event => {
     event.preventDefault()
+    if(!room && !name)
+      setMessage("Please, Enter Room and User name")
+    else if(!room)
+     setMessage("Please, Enter Room name")
+    else if(!name)
+     setMessage("Please, Enter User name")
+    else
+      setMessage('')
     if (room && name) setCall(true)
   }
 
   return (
     <div>
-      <h2>&lt;Jutsu /&gt; Demo !</h2>
+      <h1>Jitsi  &#123; XELP &#125; Meet !</h1>
       
       {call ? (<Jutsu
         roomName={room}
         password={password}
         displayName={name}
+        containerStyles={{ width: '100%', height: '700px' }}
         onMeetingEnd={() => console.log('Meeting has ended')}
         loadingComponent={<p>ʕ •ᴥ•ʔ jitsi is loading ...</p>} />)
         : (
@@ -28,9 +38,11 @@ const App = () => {
             <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
             <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
             <input id='password' type='text' placeholder='Password (optional)' value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleClick} type='submit'>
+           <br></br> <button onClick={handleClick} type='submit'>
               Start / Join
-            </button>
+            </button><br/>
+            <p style={{color:'red'}}>{message}</p>
+            
           </form>
         )}
       
